d346d69ba0d37482acd4d5d8b40d332a1491e4b3
Brad Zacher <brad.zacher@gmail.com>
2016-11-29 18:06:27 +1030

    added jsx support to eslint

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,14 @@
-import styles from './index.scss';
 import React from 'react';
+import styles from './index.scss';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
-        <p>Enjoy!</p>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>It Works!</h1>
+                <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
+                <p>Enjoy!</p>
+            </div>
+        );
+    }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './app.jsx';
 
-render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
+render(<AppContainer><App/></AppContainer>, document.querySelector('#app'));
 
 if (module && module.hot) {
-  module.hot.accept('./app.jsx', () => {
-    const App = require('./app.jsx').default;
-    render(
-      <AppContainer>
-        <App/>
-      </AppContainer>,
-      document.querySelector("#app")
-    );
-  });
+    module.hot.accept('./app.jsx', () => {
+        // eslint-disable-next-line global-require
+        const hotApp = require('./app.jsx').default;
+        render(<AppContainer><hotApp/></AppContainer>, document.querySelector('#app'));
+    });
 }
