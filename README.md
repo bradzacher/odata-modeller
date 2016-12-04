86127649f99beedfd3ac5e1721537ca10b739653
Brad Zacher <brad.zacher@gmail.com>
2016-12-05 10:28:23 +1030

    adding first gh-pages build

--- README.md
+++
@@ -1,5 +0,0 @@
-# odata-modeller
-
-A tool that will read an OData Service's metadata XML, and will generate a nice graphical representation of it.
-
-Useful for creating documentation, or for help in understanding how a service's entities are structured.
--- src/flux/loggerMiddleware.js
+++
@@ -1,8 +0,0 @@
-export default function ({ getState }) {
-    return next => (action) => {
-        console.log('will dispatch', action);
-        const returnValue = next(action);
-        console.log('state after dispatch', getState());
-        return returnValue;
-    };
-}
