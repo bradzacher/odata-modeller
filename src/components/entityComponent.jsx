7b6277bac1c1c020d517117ab0b13fcf482def2f
Brad Zacher <brad.zacher@gmail.com>
2016-12-05 12:45:15 +1030

    added proper sizing to entities

--- src/components/entityComponent.jsx
+++ src/components/entityComponent.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import extend from 'extend';
 
 import PropertyComponent from './propertyComponent.jsx';
 
 const defaultWidth = 200;
 const initialBuffer = 24;
+const minWidth = 100;
 
 export default class EntityComponent extends React.Component {
     constructor(props) {
@@ -13,7 +15,20 @@ export default class EntityComponent extends React.Component {
             top: 0,
             left: props.index * (defaultWidth + initialBuffer),
             width: defaultWidth,
+            height: null,
+            minHeight: null,
+            minWidth,
         };
+
+        // reference to the container div in the DOM
+        this.container = null;
+    }
+
+    componentDidMount() {
+        // save the browser calculated height
+        this.setState({
+            minHeight: this.container.clientHeight,
+        });
     }
 
     render() {
@@ -23,18 +38,22 @@ export default class EntityComponent extends React.Component {
 
         const properties = this.props.entity.properties.map((p, i) => (<PropertyComponent property={p} key={i} />));
 
-        const containerStyles = {
+        const containerStyles = extend({
             border: '1px solid black',
             position: 'absolute',
             overflow: 'hidden',
             top: `${this.state.top}px`,
             left: `${this.state.left}px`,
             width: `${this.state.width}px`,
-        };
+            height: this.state.height ? `${this.state.height}px` : 'auto',
+            minHeight: `${this.state.minHeight}px`,
+            // todo change this to a js based solution as it's not supported on IE/Edge
+            resize: 'both',
+        }, this.props.style);
 
         return (
-            <div style={containerStyles}>
-                <div style={{ fontWeight: 'bold', border: '1px solid black' }}>{this.props.entity.name}</div>
+            <div style={containerStyles} ref={div => this.container = div}>
+                <div style={{ fontWeight: 'bold', border: '1px solid black', cursor: 'move' }}>{this.props.entity.name}</div>
                 {properties}
             </div>
         );
@@ -44,8 +63,10 @@ export default class EntityComponent extends React.Component {
 EntityComponent.defaultProps = {
     entity: null,
     index: 0,
+    style: {},
 };
 EntityComponent.propTypes = {
     entity: React.PropTypes.object,
     index: React.PropTypes.number,
+    style: React.PropTypes.object,
 };
